b88c08ef6a9a6032e2536539747821c1a5e8e892
Anthony Ettinger <anthony@chovy.com>
2017-06-09 18:15:31 -0700

    add support for 'Hearst Jr., William Randolph'

--- index.js
+++ index.js
@@ -3,173 +3,180 @@ var _ = require('underscore');
 var parser = module.exports = {};
 
 parser.parseName = function (name) {
-    var salutations = ['mr', 'master', 'mister', 'mrs', 'miss', 'ms', 'dr', 'prof', 'rev', 'fr', 'judge', 'honorable', 'hon'];
-    var suffixes = ['i', 'ii', 'iii', 'iv', 'v', 'senior', 'junior', 'jr', 'sr', 'phd', 'apr', 'rph', 'pe', 'md', 'ma', 'dmd', 'cme'];
-    var compound = ['vere', 'von', 'van', 'de', 'del', 'della', 'der', 'di', 'da', 'pietro', 'vanden', 'du', 'st.', 'st', 'la', 'lo', 'ter', 'bin', 'ibn', 'te', 'ten', 'op', 'ben'];
+	var salutations = ['mr', 'master', 'mister', 'mrs', 'miss', 'ms', 'dr', 'prof', 'rev', 'fr', 'judge', 'honorable', 'hon'];
+	var suffixes = ['i', 'ii', 'iii', 'iv', 'v', 'senior', 'junior', 'jr', 'sr', 'phd', 'apr', 'rph', 'pe', 'md', 'ma', 'dmd', 'cme'];
+	var compound = ['vere', 'von', 'van', 'de', 'del', 'della', 'der', 'di', 'da', 'pietro', 'vanden', 'du', 'st.', 'st', 'la', 'lo', 'ter', 'bin', 'ibn', 'te', 'ten', 'op', 'ben'];
 
-    var parts = name.trim().split(/\s+/);
-		var attrs = {};
+	var parts = name.trim().split(/\s+/);
+	var attrs = {};
 
-    if (!parts.length) {
-        return attrs;
-    }
+	if (!parts.length) {
+		return attrs;
+	}
 
-    if ( parts.length === 1 ) {
-        attrs.firstName = parts[0];
-    }
+	if (parts.length === 1) {
+		attrs.firstName = parts[0];
+	}
 
-    //handle suffix first always, remove trailing comma if there is one
-    if ( parts.length > 1 && _.indexOf(suffixes, _.last(parts).toLowerCase().replace(/\./g, '')) > -1) {
-        attrs.suffix = parts.pop();
-        parts[parts.length-1] = _.last(parts).replace(',', '');
-    }
+	//handle suffix first always, remove trailing comma if there is one
+	if (parts.length > 1 && _.indexOf(suffixes, _.last(parts).toLowerCase().replace(/\./g, '')) > -1) {
+		attrs.suffix = parts.pop();
+		parts[parts.length - 1] = _.last(parts).replace(',', '');
+	}
 
-    //look for a comma to know we have last name first format
-    var firstNameFirstFormat = _.every(parts, function(part) {
-        return part.indexOf(',') === -1;
-    })
+	//look for a comma to know we have last name first format
+	var firstNameFirstFormat = _.every(parts, function (part) {
+		return part.indexOf(',') === -1;
+	})
 
-    if (!firstNameFirstFormat) {
-    //last name first format
-    //assuming salutations are never used in this format
+	if (!firstNameFirstFormat) {
+		//last name first format
+		//assuming salutations are never used in this format
 
-        //tracker variable for where first name begins in parts array
-        var firstNameIndex;
+		//tracker variable for where first name begins in parts array
+		var firstNameIndex;
 
-        //location of first comma will separate last name from rest
-        //join all parts leading to first comma as last name
-        var lastName = _.reduce(parts, function (lastName, current, index) {
-            if (!Array.isArray(lastName)) {
-                return lastName;
-            }
-            if (current.indexOf(',') === -1) {
-                lastName.push(current)
-                return lastName;
-            } else {
-                current = current.replace(',', '');
-                lastName.push(current);
-                firstNameIndex = index + 1;
-                return lastName.join(' ');
-            }
-        }, []);
+		//location of first comma will separate last name from rest
+		//join all parts leading to first comma as last name
+		var lastName = _.reduce(parts, function (lastName, current, index) {
+			if (!Array.isArray(lastName)) {
+				return lastName;
+			}
+			if (current.indexOf(',') === -1) {
+				lastName.push(current)
+				return lastName;
+			} else {
+				current = current.replace(',', '');
 
-        attrs.lastName = lastName;
+				// handle case where suffix is included in part of last name (ie: 'Hearst Jr., Willian Randolph')
+				if (_.indexOf(suffixes, current.toLowerCase().replace(/\./g, '')) > -1) {
+					attrs.suffix = current;
+				} else {
+					lastName.push(current);
+				}
 
-        var remainingParts = parts.slice(firstNameIndex);
-        if (remainingParts.length > 1) {
-            attrs.firstName = remainingParts.shift();
-            attrs.middleName = remainingParts.join(' ');
-        } else if (remainingParts.length) {
-            attrs.firstName = remainingParts[0];
-        }
+				firstNameIndex = index + 1;
+				return lastName.join(' ');
+			}
+		}, []);
 
-        //create full name from attrs object
-        var nameWords = [];
-        if (attrs.firstName) {
-            nameWords.push(attrs.firstName);
-        }
-        if (attrs.middleName) {
-            nameWords.push(attrs.middleName)
-        }
-        nameWords.push(attrs.lastName)
-        if (attrs.suffix) {
-            nameWords.push(attrs.suffix);
-        }
-        attrs.fullName = nameWords.join(' ');
+		attrs.lastName = lastName;
 
-        
-    } else {
-    //first name first format
+		var remainingParts = parts.slice(firstNameIndex);
+		if (remainingParts.length > 1) {
+			attrs.firstName = remainingParts.shift();
+			attrs.middleName = remainingParts.join(' ');
+		} else if (remainingParts.length) {
+			attrs.firstName = remainingParts[0];
+		}
+
+		//create full name from attrs object
+		var nameWords = [];
+		if (attrs.firstName) {
+			nameWords.push(attrs.firstName);
+		}
+		if (attrs.middleName) {
+			nameWords.push(attrs.middleName)
+		}
+		nameWords.push(attrs.lastName)
+		if (attrs.suffix) {
+			nameWords.push(attrs.suffix);
+		}
+		attrs.fullName = nameWords.join(' ');
 
 
-        if ( parts.length > 1 && _.indexOf(salutations, _.first(parts).toLowerCase().replace(/\./g, '')) > -1) {
-            attrs.salutation = parts.shift();
-            attrs.firstName = parts.shift();
-        } else {
-            attrs.firstName = parts.shift();
-        }
+	} else {
+		//first name first format
 
-        attrs.lastName  = parts.length ? parts.pop() : '';
 
-        // test for compound last name, we reverse because middle name is last bit to be defined.
-        // We already know lastname, so check next word if its part of a compound last name.
-        var revParts = parts.slice(0).reverse();
-				var compoundParts = [];
+		if (parts.length > 1 && _.indexOf(salutations, _.first(parts).toLowerCase().replace(/\./g, '')) > -1) {
+			attrs.salutation = parts.shift();
+			attrs.firstName = parts.shift();
+		} else {
+			attrs.firstName = parts.shift();
+		}
 
-        _.every(revParts, function(part, i, all){
-            var test = part.toLowerCase().replace(/\./g, '');
+		attrs.lastName = parts.length ? parts.pop() : '';
 
-            if (_.indexOf(compound, test) > -1 ) {
-                compoundParts.push(part);
+		// test for compound last name, we reverse because middle name is last bit to be defined.
+		// We already know lastname, so check next word if its part of a compound last name.
+		var revParts = parts.slice(0).reverse();
+		var compoundParts = [];
 
-                return true;
-            }
+		_.every(revParts, function (part, i, all) {
+			var test = part.toLowerCase().replace(/\./g, '');
 
-            //break on first non compound word
-            return false;
-        });
+			if (_.indexOf(compound, test) > -1) {
+				compoundParts.push(part);
 
-        //join compound parts with known last name
-        if ( compoundParts.length ) {
-            attrs.lastName = compoundParts.reverse().join(' ') + ' ' + attrs.lastName;
+				return true;
+			}
 
-            parts = _.difference(parts, compoundParts);
-        }
+			//break on first non compound word
+			return false;
+		});
 
-        if ( parts.length ) {
-            attrs.middleName = parts.join(' ');
-        }
+		//join compound parts with known last name
+		if (compoundParts.length) {
+			attrs.lastName = compoundParts.reverse().join(' ') + ' ' + attrs.lastName;
 
-        //remove comma like "<lastName>, Jr."
-        if ( attrs.lastName ) {
-            attrs.lastName = attrs.lastName.replace(',', '');
-        }
+			parts = _.difference(parts, compoundParts);
+		}
 
-        //save a copy of original
-        attrs.fullName = name;
+		if (parts.length) {
+			attrs.middleName = parts.join(' ');
+		}
 
-    }
-    //console.log('attrs:', JSON.stringify(attrs));
-    return attrs;
+		//remove comma like "<lastName>, Jr."
+		if (attrs.lastName) {
+			attrs.lastName = attrs.lastName.replace(',', '');
+		}
+
+		//save a copy of original
+		attrs.fullName = name;
+
+	}
+	//console.log('attrs:', JSON.stringify(attrs));
+	return attrs;
 };
 
-parser.getFullestName = function(str){
-    var name = str;
-		var names = [];
+parser.getFullestName = function (str) {
+	var name = str;
+	var names = [];
 
-    //find fullname from strings like 'Jon and Sue Doyle'
-    if ( name.indexOf('&') > -1 || name.toLowerCase().indexOf(' and ') > -1 ) {
-        names = name.split(/\s+(?:and|&)\s+/gi);
+	//find fullname from strings like 'Jon and Sue Doyle'
+	if (name.indexOf('&') > -1 || name.toLowerCase().indexOf(' and ') > -1) {
+		names = name.split(/\s+(?:and|&)\s+/gi);
 
-        //pluck the name with the most parts (first, middle, last) from the array.
-        //will grab 'Sue Doyle' in 'Jon & Sue Anne Doyle'
-        if ( names.length ) {
-            name = names.sort(function(a, b) {
-                return b.split(/\s+/).length - a.split(/\s+/).length;
-            })[0];
-        }
-    }
+		//pluck the name with the most parts (first, middle, last) from the array.
+		//will grab 'Sue Doyle' in 'Jon & Sue Anne Doyle'
+		if (names.length) {
+			name = names.sort(function (a, b) {
+				return b.split(/\s+/).length - a.split(/\s+/).length;
+			})[0];
+		}
+	}
 
-    return name;
+	return name;
 };
 
-parser.parseAddress = function(str){
-    //416 W. Manchester Blvd., Inglewood, CA  90301
-    var parts = str.split(/,\s+/).reverse();
-		var stateZip;
-		var city;
-		var address = {};
+parser.parseAddress = function (str) {
+	//416 W. Manchester Blvd., Inglewood, CA  90301
+	var parts = str.split(/,\s+/).reverse();
+	var stateZip;
+	var city;
+	var address = {};
 
-    stateZip = parts[0].split(/\s+/);
-    parts.shift();
+	stateZip = parts[0].split(/\s+/);
+	parts.shift();
 
-    city = parts.shift();
+	city = parts.shift();
 
-    address.address = parts.reverse().join(', ');
-    address.city = city;
-    address.state = stateZip[0];
-    address.zip = stateZip[1];
-    address.fullAddress = str;
+	address.address = parts.reverse().join(', ');
+	address.city = city;
+	address.state = stateZip[0];
+	address.zip = stateZip[1];
+	address.fullAddress = str;
 
-    return address;
+	return address;
 };
--- test/index.js
+++ test/index.js
@@ -14,60 +14,60 @@ var human = require('..');
 
 
 // `describe` makes a "suite" of tests; think of them as a group.
-describe('Parsing names', function() {
+describe('Parsing names', function () {
 
-    var names = [
-        {
-            name: 'Mr. William R. Hearst, III',
-            result: {
-                salutation: 'Mr.',
-                firstName: 'William',
-                middleName: 'R.',
-                lastName: 'Hearst',
-                suffix: 'III',
-                fullName: 'Mr. William R. Hearst, III'
-            }
-        },{
-            name: 'William Randolph Hearst',
-            result: {
-                firstName: 'William',
-                lastName: 'Hearst',
-                middleName: 'Randolph',
-                fullName: 'William Randolph Hearst'
-            }
-        }, {
-            name: 'William R. De La Cruz',
-            result: {
-                firstName: 'William',
-                lastName: 'De La Cruz',
-                middleName: 'R.',
-                fullName: 'William R. De La Cruz'
-            }
-        }, {
-            name: 'Mr. William R. De La Cruz III',
-            result: {
-                salutation: 'Mr.',
-                firstName: 'William',
-                suffix: 'III',
-                lastName: 'De La Cruz',
-                middleName: 'R.',
-                fullName: 'Mr. William R. De La Cruz III'
-            }
-        }, {
-            name: 'William De Cruz',
-            result: {
-                firstName: 'William',
-                lastName: 'De Cruz',
-                fullName: 'William De Cruz'
-            }
-        }, {
-            name: 'William De La Cruz',
-            result: {
-                firstName: 'William',
-                lastName: 'De La Cruz',
-                fullName: 'William De La Cruz'
-            }
-        }, {
+	var names = [
+		{
+			name: 'Mr. William R. Hearst, III',
+			result: {
+				salutation: 'Mr.',
+				firstName: 'William',
+				middleName: 'R.',
+				lastName: 'Hearst',
+				suffix: 'III',
+				fullName: 'Mr. William R. Hearst, III'
+			}
+		}, {
+			name: 'William Randolph Hearst',
+			result: {
+				firstName: 'William',
+				lastName: 'Hearst',
+				middleName: 'Randolph',
+				fullName: 'William Randolph Hearst'
+			}
+		}, {
+			name: 'William R. De La Cruz',
+			result: {
+				firstName: 'William',
+				lastName: 'De La Cruz',
+				middleName: 'R.',
+				fullName: 'William R. De La Cruz'
+			}
+		}, {
+			name: 'Mr. William R. De La Cruz III',
+			result: {
+				salutation: 'Mr.',
+				firstName: 'William',
+				suffix: 'III',
+				lastName: 'De La Cruz',
+				middleName: 'R.',
+				fullName: 'Mr. William R. De La Cruz III'
+			}
+		}, {
+			name: 'William De Cruz',
+			result: {
+				firstName: 'William',
+				lastName: 'De Cruz',
+				fullName: 'William De Cruz'
+			}
+		}, {
+			name: 'William De La Cruz',
+			result: {
+				firstName: 'William',
+				lastName: 'De La Cruz',
+				fullName: 'William De La Cruz'
+			}
+		}, {
 			name: 'Mr. William R. Hugh Calum De La Cruz III',
 			result: {
 				salutation: 'Mr.',
@@ -102,132 +102,141 @@ describe('Parsing names', function() {
 				fullName: 'Kiefer William Frederick Dempsey George Rufus Sutherland'
 			}
 		}, {
-            name: 'William Hearst',
-            result: {
-                firstName: 'William',
-                lastName: 'Hearst',
-                fullName: 'William Hearst'
-            }
-        }, {
-            name: 'William Hearst Jr',
-            result: {
-                firstName: 'William',
-                suffix: 'Jr',
-                lastName: 'Hearst',
-                fullName: 'William Hearst Jr'
-            }
-        }, {
-            name: 'Hearst, William Jr',
-            result: {
-                firstName: 'William',
-                suffix: 'Jr',
-                lastName: 'Hearst',
-                fullName: 'William Hearst Jr'
-            }
-        }, {
-            name: 'Hearst, William Randolph',
-            result: {
-                firstName: 'William',
-                lastName: 'Hearst',
-                middleName: 'Randolph',
-                fullName: 'William Randolph Hearst'
-            }
-        }, {
-            name: 'Hearst, William, M.D.',
-            result: {
-                firstName: 'William',
-                suffix: 'M.D.',
-                lastName: 'Hearst',
-                fullName: 'William Hearst M.D.'
-            }
-        }, {
-            name: 'William',
-            result: {
-                firstName: 'William',
-                lastName: '',
-                fullName: 'William'
-            }
-        }, {
-            name: '',
-            result: {
-                firstName: '',
-                lastName: '',
-                fullName: ''
-            }
-        }
-    ];
+			name: 'William Hearst',
+			result: {
+				firstName: 'William',
+				lastName: 'Hearst',
+				fullName: 'William Hearst'
+			}
+		}, {
+			name: 'William Hearst Jr',
+			result: {
+				firstName: 'William',
+				suffix: 'Jr',
+				lastName: 'Hearst',
+				fullName: 'William Hearst Jr'
+			}
+		}, {
+			name: 'Hearst, William Jr',
+			result: {
+				firstName: 'William',
+				suffix: 'Jr',
+				lastName: 'Hearst',
+				fullName: 'William Hearst Jr'
+			}
+		}, {
+			name: 'Hearst, William Randolph',
+			result: {
+				firstName: 'William',
+				lastName: 'Hearst',
+				middleName: 'Randolph',
+				fullName: 'William Randolph Hearst'
+			}
+		}, {
+			name: 'Hearst Jr., William Randolph',
+			result: {
+				firstName: 'William',
+				lastName: 'Hearst',
+				middleName: 'Randolph',
+				suffix: 'Jr.',
+				fullName: 'William Randolph Hearst Jr.'
+			}
+		}, {
+			name: 'Hearst, William, M.D.',
+			result: {
+				firstName: 'William',
+				suffix: 'M.D.',
+				lastName: 'Hearst',
+				fullName: 'William Hearst M.D.'
+			}
+		}, {
+			name: 'William',
+			result: {
+				firstName: 'William',
+				lastName: '',
+				fullName: 'William'
+			}
+		}, {
+			name: '',
+			result: {
+				firstName: '',
+				lastName: '',
+				fullName: ''
+			}
+		}
+	];
 
-    var fullest = [
-        {
-            name: 'John & Peggy Sue',
-            result: {
-                fullName: 'Peggy Sue'
-            }
-        }, {
-            name: 'John and Peggy Sue',
-            result: {
-                fullName: 'Peggy Sue'
-            }
-        }, {
-            name: 'Jane and Mr. William R. De La Cruz III',
-            result: {
-                fullName: 'Mr. William R. De La Cruz III'
-            }
-        }
-    ];
+	var fullest = [
+		{
+			name: 'John & Peggy Sue',
+			result: {
+				fullName: 'Peggy Sue'
+			}
+		}, {
+			name: 'John and Peggy Sue',
+			result: {
+				fullName: 'Peggy Sue'
+			}
+		}, {
+			name: 'Jane and Mr. William R. De La Cruz III',
+			result: {
+				fullName: 'Mr. William R. De La Cruz III'
+			}
+		}
+	];
 
-    var addresses = [
-        {
-            address: '123 W. Happy Day Blvd., San Francisco, CA  90501',
-            result: {
-                address: '123 W. Happy Day Blvd.',
-                city: 'San Francisco',
-                state: 'CA',
-                zip: '90501',
-                fullAddress: '123 W. Happy Day Blvd., San Francisco, CA  90501'
-            }
-        }, {
-            address: '123 Happy Street, Honolulu, HI  65780',
-            result: {
-                address: '123 Happy Street',
-                city: 'Honolulu',
-                state: 'HI',
-                zip: '65780',
-                fullAddress: '123 Happy Street, Honolulu, HI  65780'
-            }
-        }, {
-            address: '123 Happy Street, Suite #101, Honolulu, HI  65780',
-            result: {
-                address: '123 Happy Street, Suite #101',
-                city: 'Honolulu',
-                state: 'HI',
-                zip: '65780',
-                fullAddress: '123 Happy Street, Suite #101, Honolulu, HI  65780'
-            }
-        }
-    ];
+	var addresses = [
+		{
+			address: '123 W. Happy Day Blvd., San Francisco, CA  90501',
+			result: {
+				address: '123 W. Happy Day Blvd.',
+				city: 'San Francisco',
+				state: 'CA',
+				zip: '90501',
+				fullAddress: '123 W. Happy Day Blvd., San Francisco, CA  90501'
+			}
+		}, {
+			address: '123 Happy Street, Honolulu, HI  65780',
+			result: {
+				address: '123 Happy Street',
+				city: 'Honolulu',
+				state: 'HI',
+				zip: '65780',
+				fullAddress: '123 Happy Street, Honolulu, HI  65780'
+			}
+		}, {
+			address: '123 Happy Street, Suite #101, Honolulu, HI  65780',
+			result: {
+				address: '123 Happy Street, Suite #101',
+				city: 'Honolulu',
+				state: 'HI',
+				zip: '65780',
+				fullAddress: '123 Happy Street, Suite #101, Honolulu, HI  65780'
+			}
+		}
+	];
 
-    it('Should parse all name attributes', function() {
-        names.forEach(function(name, i, list){
-            var parsed = human.parseName(name.name);
+	it('Should parse all name attributes', function () {
+		names.forEach(function (name, i, list) {
+			var parsed = human.parseName(name.name);
 
-            expect(name.result).to.eql(parsed);
-        });
-    });
+			expect(name.result).to.eql(parsed);
+		});
+	});
 
-    it('Should parse fullest name', function() {
-        fullest.forEach(function(name, i, list){
-            var fullName = human.getFullestName(name.name);
+	it('Should parse fullest name', function () {
+		fullest.forEach(function (name, i, list) {
+			var fullName = human.getFullestName(name.name);
 
-            expect(name.result.fullName).to.eql(fullName);
-        });
-    });
+			expect(name.result.fullName).to.eql(fullName);
+		});
+	});
 
-    it('Should parse all address attributes', function() {
-        addresses.forEach(function(address, i, list){
-            var parsed = human.parseAddress(address.address);
+	it('Should parse all address attributes', function () {
+		addresses.forEach(function (address, i, list) {
+			var parsed = human.parseAddress(address.address);
 
-            expect(address.result).to.eql(parsed);
-        });
-    });
+			expect(address.result).to.eql(parsed);
+		});
+	});
 });
